f76f967c1cfc3ea16ebb78188e0a6951d7163c2e
GMarcell <42812913+GMarcell@users.noreply.github.com>
2022-05-08 22:21:46 +0700

    Done 90%

--- js/main.js
+++ js/main.js
@@ -1,42 +1,12 @@
-/* Set the width of the side navigation to 250px */
-function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-}
+const ham = document.querySelector('.ham')
 
-  /* Set the width of the side navigation to 0 */
-function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-}
-
-//* Loop through all dropdown buttons to toggle between hiding and showing its dropdown content - This allows the user to have multiple dropdowns without any conflict */
-var dropdown = document.getElementsByClassName("dropdown-btn");
-var i;
-
-for (i = 0; i < dropdown.length; i++) {
-    dropdown[i].addEventListener("click", function() {
-        this.classList.toggle("active");
-        var dropdownContent = this.nextElementSibling;
-        if (dropdownContent.style.display === "block") {
-            dropdownContent.style.display = "none";
-        } else {
-            dropdownContent.style.display = "block";
-        }
-    });
-}
-
-function changeImgfeature() {
-    var image = document.getElementById('arrowfeature')
-    if (image.src.match("/images/icon-arrow-down.svg")){
-        image.src = "/images/icon-arrow-up.svg"
-    }else {
-        image.src = "/images/icon-arrow-down.svg"
-    }
-}
-function changeImgcompany() {
-    var image = document.getElementById('arrowcompany')
-    if (image.src.match("/images/icon-arrow-down.svg")){
-        image.src = "/images/icon-arrow-up.svg"
-    }else {
-        image.src = "/images/icon-arrow-down.svg"
-    }
-}
+  ham.addEventListener('click', e=>{
+    document.querySelector('.mobile-nav').classList.add('show')
+    document.querySelector('.overlay').style.display = 'block'
+    document.querySelector('body').classList.add('lock')
+  })
+  document.querySelector('#close-Menu').addEventListener('click', e =>{
+    document.querySelector('.mobile-nav').classList.remove('show')
+    document.querySelector('.overlay').style.display = 'none'
+    document.querySelector('body').classList.remove('lock')
+  })
